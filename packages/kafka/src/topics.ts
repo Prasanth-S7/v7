4666abe66c864985dabc3d6e9cac71efa03727bb
Prasanth <prasanthsampath2005@gmail.com>
2026-07-24 20:10:18 +0530

    feat: added a few more topic constants

--- packages/kafka/src/topics.ts
+++ packages/kafka/src/topics.ts
@@ -1,4 +1,10 @@
 export const Topics = {
     CREATE_PROJECT: "create.project",
     PROJECT_CREATED: "project.created",
+    PROMPT: "prompt",
+    PROMPT_RESPONSE: "prompt.response",
+    BUILD_PROJECT: "build.project", 
+    PROJECT_BUILT: "project.built",
+    RUN_PROJECT: "run.project",
+    PROJECT_RUNNING: "project.running"
 }
